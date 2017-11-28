52fb71480d825b4e57419dbe7c8f6831c1ec6247
Andy Edwards <jedwards@fastmail.com>
2017-11-28 12:52:19 -0600

    fix(flow): upgrade flow to version 0.56.0
    BREAKING CHANGE: flow types may have been affected and incompatible with
    prior versions of flow.

--- src/withTransitionContext.js
+++ src/withTransitionContext.js
@@ -1,12 +1,12 @@
 // @flow
 
-import React from 'react'
+import * as React from 'react'
 import {createFader, defaultWrapChildren} from './index'
 import TransitionContext from 'react-transition-context'
 
 type TransitionState = 'in' | 'out' | 'entering' | 'leaving'
 
-function wrapChildren(children: any, transitionState: TransitionState): React.Element<any> {
+function wrapChildren(children: any, transitionState: TransitionState): React.Element<typeof TransitionContext> {
   return defaultWrapChildren.call(
     this,
     <TransitionContext transitionState={transitionState}>
