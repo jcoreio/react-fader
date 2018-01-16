83eeb7f00dc1c45eb9c297c6f6ec71c4782049d1
Andy Edwards <jedwards@fastmail.com>
2018-01-16 02:25:32 -0600

    merge changes from skeleton and upgrade various things

--- test/index.js
+++ test/index.js
@@ -1,19 +1,16 @@
-import React from 'react'
+// @flow
+
+import * as React from 'react'
 import {mount} from 'enzyme'
 import {expect} from 'chai'
 
-import { before } from 'mocha'
-import { configure } from 'enzyme'
+import {describe, it} from 'mocha'
+import {configure} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-configure({ adapter: new Adapter() })
-
-/* eslint-env node */
-
-if (process.argv.indexOf('--watch') >= 0) {
-  before(() => process.stdout.write('\u001b[2J\u001b[1;1H\u001b[3J'))
-}
 
 import Hello from '../src/index'
+
+configure({ adapter: new Adapter() })
 
 describe('test setup', () => {
   it('works', () => {
